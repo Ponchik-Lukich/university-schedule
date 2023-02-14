4fbfe061585dc396d9ff104839968425d922f3e0
Artemii <artem-lukichev@bk.ru>
2023-02-14 16:53:15 +0300

    checking with hash

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"io"
 	"net/http"
-	"strings"
 	"time"
 )
 
 func main() {
 	url := "https://time.is/"
-	var prevContent string
+	var prevContent [16]byte
 
 	for {
-		content, err := fetchContent(url)
+		content, err := calculateHash(url)
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -33,26 +32,20 @@ func main() {
 	}
 }
 
-func fetchContent(url string) (string, error) {
+func calculateHash(url string) ([16]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		return [16]byte{}, err
 	}
 
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return [16]byte{}, err
 	}
 
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
-	if err != nil {
-		return "", err
-	}
+	hash := md5.Sum(body)
 
-	// Extract the relevant information from the HTML using goquery selectors
-	content := doc.Text()
-
-	return content, nil
+	return hash, nil
 }
