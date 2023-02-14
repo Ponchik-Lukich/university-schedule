d28e1ec50aabc0231c164cf4efb29a44b4a4857f
Artemii <artem-lukichev@bk.ru>
2023-02-14 18:06:51 +0300

    some changes

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
+	"sync"
 )
 
 var websites = []string{
@@ -13,36 +13,34 @@ var websites = []string{
 	"https://home.potatohd.ru/departments/2266/exams",
 }
 
+var wg sync.WaitGroup
+
 func main() {
-	// create [16]byte prevContent array for each website
 	prevContent := make([][16]byte, len(websites))
 	for i := 0; i < len(websites); i++ {
 		prevContent = append(prevContent, [16]byte{})
 	}
 
-	// check each website in a loop parallel
 	for i, url := range websites {
+		wg.Add(1)
 		go func(i int, url string) {
-			for {
-				content, err := calculateHash(url)
-				if err != nil {
-					println(err)
-					continue
-				}
-
-				if prevContent[i] != content {
-					fmt.Printf("Website %s changed!\n", url)
-				} else {
-					fmt.Printf("Nothing on %s\n", url)
-				}
-
-				prevContent[i] = content
-				time.Sleep(time.Minute)
+			defer wg.Done()
+			content, err := calculateHash(url)
+			if err != nil {
+				println(err)
+				return
 			}
+
+			if prevContent[i] != content {
+				fmt.Printf("Website %s changed!\n", url)
+			} else {
+				fmt.Printf("Nothing on %s\n", url)
+			}
+
+			prevContent[i] = content
 		}(i, url)
 	}
-	//time.Sleep(time.Minute)
-	select {}
+	wg.Wait()
 }
 
 func calculateHash(url string) ([16]byte, error) {
