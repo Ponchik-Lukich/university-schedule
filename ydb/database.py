adf479763f033aef79039a4d1735b0360a717ffe
Artemii <artem-lukichev@bk.ru>
2023-02-16 21:11:26 +0300

    nevermind

--- ydb/database.py
+++ ydb/database.py
@@ -1,12 +1,15 @@
 # from yandex.cloud import ydb
 
 import ydb.iam
+import os
+from dotenv import load_dotenv
 # from ydb.aio import driver
 
+load_dotenv()
 
 driver_config = {
-    "endpoint": "grpcs://ydb.serverless.yandexcloud.net:2135",
-    "database": "/ru-central1/b1gsopf4nf64thcp1nbp/etnnv6lqhm7e2vaqgk89",
+    "endpoint": os.getenv('ENDPOINT'),
+    "database": os.getenv('DATABASE'),
     # "credentials": ydb.AnonymousCredentials()
     "credentials": ydb.iam.ServiceAccountCredentials.from_file(
         "./authorized_key.json"
@@ -25,6 +28,7 @@ with ydb.Driver(**driver_config) as driver:
                              .with_column(ydb.Column('series_info', ydb.OptionalType(ydb.PrimitiveType.Utf8)))
                              .with_column(ydb.Column('release_date', ydb.OptionalType(ydb.PrimitiveType.Uint64)))
                              .with_primary_key('series_id'))
+        exit(1)
     except TimeoutError:
         print("Connect failed to YDB")
         print("Last reported errors by discovery:")
