7a948b82785a08b00a2df67bb18d7c60c68b4108
Artemii <artem-lukichev@bk.ru>
2023-02-17 21:49:59 +0300

    save

--- main.go
+++ main.go
@@ -2,42 +2,68 @@ package main
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"io"
 	"net/http"
+	"strings"
 	"sync"
+	"time"
 )
 
 var websites = []string{
 	"https://time.is/",
-	"https://home.potatohd.ru/departments/2266/exams",
+}
+
+var prevContent = []string{
+	"",
+}
+
+var prevXpath = []string{
+	"",
 }
 
 var wg sync.WaitGroup
 
 func main() {
-	prevContent := make([][16]byte, len(websites))
-	for i := 0; i < len(websites); i++ {
-		prevContent = append(prevContent, [16]byte{})
-	}
 
 	for i, url := range websites {
 		wg.Add(1)
 		go func(i int, url string) {
 			defer wg.Done()
-			content, err := calculateHash(url)
-			if err != nil {
-				println(err)
-				return
+			result := false
+			data, _ := getWebsiteData(url)
+			doc, _ := goquery.NewDocumentFromReader(strings.NewReader(data))
+			body := doc.Find("*").First()
+			removeJunk(body)
+			if prevXpath[i] != "" {
+				body = getXpathData(body, prevXpath[i])
+			}
+			data = body.Text()
+			hash, _ := getWebsiteHash(data)
+			if hash != prevContent[i] {
+				prevContent[i] = hash
+				result = true
 			}
 
-			if prevContent[i] != content {
+			//content, err := calculateHash(url)
+			//fmt.Println(content)
+			//if err != nil {
+			//	println(err)
+			//	return
+			//}
+			//
+			fmt.Println(hash)
+			if result {
 				fmt.Printf("Website %s changed!\n", url)
 			} else {
 				fmt.Printf("Nothing on %s\n", url)
 			}
-
-			prevContent[i] = content
+			//
+			//prevContent[i] = content
 		}(i, url)
 	}
 	wg.Wait()
@@ -55,8 +81,75 @@ func calculateHash(url string) ([16]byte, error) {
 	if err != nil {
 		return [16]byte{}, err
 	}
+	fmt.Println(resp.Body)
 
 	hash := md5.Sum(body)
 
 	return hash, nil
+}
+
+func getWebsiteHash(data string) (string, error) {
+	hasher := sha1.New()
+	_, err := hasher.Write([]byte(data))
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(hasher.Sum(nil)), nil
+}
+
+func getWebsiteData(url string) (string, error) {
+	method := "GET"
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		return "", err
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic("Error closing body. Here's why: " + err.Error())
+		}
+	}(res.Body)
+	if res.Status != "200 OK" {
+		return "", errors.New("status code is not 200")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func removeJunk(data *goquery.Selection) {
+	// get attributes
+	//println(data.Attr("class"))
+	data = data.RemoveAttr("class")
+	//println(data.Attr("class"))
+	data = data.RemoveAttr("id")
+	data.Contents().Each(func(i int, s *goquery.Selection) {
+		if s.Is("script") {
+			s.Remove()
+		} else if s.Is("style") {
+			s.Remove()
+		} else {
+			removeJunk(s)
+		}
+	})
+}
+func getXpathData(body *goquery.Selection, xpath string) *goquery.Selection {
+	// get only body
+	// remove classes and ids, keep only text recursively
+	// remove /html/body from xpath
+	xpath = strings.ReplaceAll(xpath, "/html/body", "")
+
+	return body.Find(xpath)
 }
